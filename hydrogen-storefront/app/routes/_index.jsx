8abce6840b1e134d3fdd4ab4c7c30dfc18b2c617
yxuan1996 <yxuan1996@gmail.com>
2023-11-15 10:50:21 +0000

    Add Index Page

---
+++ hydrogen-storefront/app/routes/_index.jsx
@@ -0,0 +1,15 @@
+export function meta() {
+    return [
+      {title: 'Hydrogen'},
+      {description: 'A custom storefront powered by Hydrogen'},
+    ];
+  }
+  
+  export default function Index() {
+    return (
+      <div>
+        <h1>Hello from the home page!</h1>
+      </div>
+    );
+  }
+  
